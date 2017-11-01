5afcd551947a32235265ee45e292474eaa5f4aa3
raiza <raizacampos2@gmail.com>
2017-10-31 22:24:18 -0200

    aula 20

---
+++ src/aula20/dcc171/Aula20Dcc171.java
@@ -0,0 +1,48 @@
+package aula20.dcc171;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Aula20Dcc171 {
+    public static void main(String[] args) {
+        try {
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+            System.out.println("Driver carregado com sucesso");
+            
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Aula20Dcc171.class.getName()).log(Level.SEVERE, "Driver de banco não disponível");
+            Logger.getLogger(Aula20Dcc171.class.getName()).log(Level.SEVERE, null, ex);
+            System.exit(-1);
+        }
+    
+    
+    String driverURL = "jdbc:derby://localhost:1527/2017-dcc171";
+    Connection conexao = null;
+    
+        try {
+            conexao = DriverManager.getConnection(driverURL, "usuario", "425570");
+        } catch (SQLException ex) {
+            Logger.getLogger(Aula20Dcc171.class.getName()).log(Level.SEVERE, null, ex);
+        }
+             
+        
+        
+        Statement operacao;
+        try {
+        operacao = conexao.createStatement();
+            operacao.executeUpdate("CREATE TABLE produto ("
+                    +"nome VARCHAR(80),"
+                    +"qtd INTEGER, "
+                    +"atualizado TIMESTAMP"
+                    +")");
+        } catch (SQLException ex) {
+            Logger.getLogger(Aula20Dcc171.class.getName()).log(Level.SEVERE, null, ex);
+        }
+            
+            
+    }   
+}
